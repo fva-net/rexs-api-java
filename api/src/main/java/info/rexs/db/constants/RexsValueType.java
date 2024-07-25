6c399e382826d0db0b13533cb6a34c7e53d2ddd7
Koenig Johannes Dr. FRD DIWG6 <johannes.koenig3@zf.com>
2024-07-25 10:14:52 +0200

    add numeric ID to RexsValueType
    part of fva-net/rexs-api-java#117

--- api/src/main/java/info/rexs/db/constants/RexsValueType.java
+++ api/src/main/java/info/rexs/db/constants/RexsValueType.java
@@ -15,6 +15,10 @@
  ******************************************************************************/
 package info.rexs.db.constants;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * This enum represents a REXS value type.
  * <p>
@@ -22,101 +26,109 @@ package info.rexs.db.constants;
  *
  * @author FVA GmbH
  */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum RexsValueType {
 
-	BOOLEAN("boolean"),
-	BOOLEAN_ARRAY("boolean_array", BOOLEAN),
-	BOOLEAN_MATRIX("boolean_matrix", BOOLEAN),
-	STRING("string"),
-	STRING_ARRAY("string_array", STRING),
-	STRING_MATRIX("string_matrix", STRING),
-	INTEGER("integer"),
-	INTEGER_ARRAY("integer_array", INTEGER),
-	INTEGER_MATRIX("integer_matrix", INTEGER),
-	FLOATING_POINT("floating_point"),
-	FLOATING_POINT_ARRAY("floating_point_array", FLOATING_POINT),
-	FLOATING_POINT_MATRIX("floating_point_matrix", FLOATING_POINT),
-	ENUM("enum"),
-	ENUM_ARRAY("enum_array", ENUM),
-	ARRAY_OF_INTEGER_ARRAYS("array_of_integer_arrays", INTEGER),
-	REFERENCE_COMPONENT("reference_component"),
-	FILE_REFERENCE("file_reference"),
-	DATE_TIME("date_time");
+    BOOLEAN(2, "boolean"),
+    BOOLEAN_ARRAY(7, "boolean_array", BOOLEAN),
+    BOOLEAN_MATRIX(16, "boolean_matrix", BOOLEAN),
+    STRING(5, "string"),
+    STRING_ARRAY(14, "string_array", STRING),
+    STRING_MATRIX(17, "string_matrix", STRING),
+    INTEGER(3, "integer"),
+    INTEGER_ARRAY(11, "integer_array", INTEGER),
+    INTEGER_MATRIX(15, "integer_matrix", INTEGER),
+    FLOATING_POINT(1, "floating_point"),
+    FLOATING_POINT_ARRAY(8, "floating_point_array", FLOATING_POINT),
+    FLOATING_POINT_MATRIX(10, "floating_point_matrix", FLOATING_POINT),
+    ENUM(4, "enum"),
+    ENUM_ARRAY(12, "enum_array", ENUM),
+    ARRAY_OF_INTEGER_ARRAYS(13, "array_of_integer_arrays", INTEGER),
+    REFERENCE_COMPONENT(9, "reference_component"),
+    FILE_REFERENCE(6, "file_reference"),
+    DATE_TIME(19, "date_time");
 
-	/** The actual key of the value type as a {@link String}. */
-	private final String key;
+    /**
+     * The numeric ID of this value type.
+     */
+    private final int numericId;
+    /**
+     * The actual key of the value type.
+     */
+    private final String key;
+    /**
+     * The base type associated with the value type.
+     */
+    private final RexsValueType basicType;
 
-	/** The base type associated with the data type. */
-	private final RexsValueType basicType;
 
-	private RexsValueType(String key, RexsValueType basicType) {
-		this.key = key;
-		this.basicType = basicType;
-	}
+    /**
+     * Constructs a new RexsValueType with the specified numeric ID and key.
+     *
+     * @param id  The numeric ID of the RexsValueType.
+     * @param key The actual key of the RexsValueType as a String.
+     */
+    RexsValueType(int id, String key) {
+        this(id, key, null);
+    }
 
-	private RexsValueType(String key) {
-		this(key, null);
-	}
 
-	/**
-	 * @return
-	 * 				The actual key of the value type as a {@link String}.
-	 */
-	public String getKey() {
-		return key;
-	}
+    /**
+     * Returns the value type for a textual key of all value types.
+     *
+     * @param key The actual key of the value type to be found as a {@link String}
+     * @return The found value type as {@link RexsValueType}, or {@code null} if the key could not be found.
+     */
+    public static RexsValueType findByKey(String key) {
+        if (key == null)
+            return null;
 
-	/**
-	 * @return
-	 * 				The base type associated with the data type.
-	 */
-	public RexsValueType getBasicType() {
-		if (basicType == null)
-			return this;
-		return basicType;
-	}
+        for (RexsValueType valueType : values()) {
+            if (key.equals(valueType.getKey()))
+                return valueType;
+        }
 
-	/**
-	 * TODO Document me!
-	 *
-	 * @param checkValueTypes
-	 * 				TODO Document me!
-	 *
-	 * @return
-	 * 				TODO Document me!
-	 */
-	public boolean isOneOf(RexsValueType ... checkValueTypes)
-	{
-		if (checkValueTypes == null)
-			return false;
+        return null;
+    }
 
-		for (RexsValueType checkValueType : checkValueTypes)
-		{
-			if (this == checkValueType)
-				return true;
-		}
+    /**
+     * Returns the value type for a numeric ID of all value types.
+     *
+     * @param numericId The numeric ID of the value type to be found.
+     * @return The found value type as {@link RexsValueType}, or {@code null} if the ID could not be found.
+     */
+    public static RexsValueType findByNumericId(int numericId) {
+        for (RexsValueType valueType : values()) {
+            if (valueType.getNumericId() == numericId)
+                return valueType;
+        }
+        return null;
+    }
 
-		return false;
-	}
+    /**
+     * @return The base type associated with the data type.
+     */
+    public RexsValueType getBasicType() {
+        if (basicType == null)
+            return this;
+        return basicType;
+    }
 
-	/**
-	 * Returns the value type for a textual key of all value types.
-	 *
-	 * @param key
-	 * 				The actual key of the value type to be found as a {@link String}
-	 *
-	 * @return
-	 * 				The found value type as {@link RexsValueType}, or {@code null} if the key could not be found.
-	 */
-	public static RexsValueType findByKey(String key) {
-		if (key == null)
-			return null;
+    /**
+     * Determines whether this RexsValueType is one of the specified RexsValueTypes.
+     *
+     * @param checkValueTypes The RexsValueTypes to check against.
+     * @return true if this RexsValueType is one of the specified types, false otherwise.
+     */
+    public boolean isOneOf(RexsValueType... checkValueTypes) {
+        if (checkValueTypes == null)
+            return false;
 
-		for (RexsValueType valueType : values()) {
-			if (key.equals(valueType.getKey()))
-				return valueType;
-		}
-
-		return null;
-	}
+        for (RexsValueType checkValueType : checkValueTypes) {
+            if (this == checkValueType)
+                return true;
+        }
+        return false;
+    }
 }
