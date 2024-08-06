ad3c12976a1ae7b725a0bc1c6a11dfa19b14bbe2
Koenig Johannes Dr. FRD DIWG6 <johannes.koenig3@zf.com>
2024-08-06 10:11:31 +0200

    Add RexsValueTypeAdapter

---
+++ api/src/main/java/info/rexs/model/jaxb/RexsValueTypeAdapter.java
@@ -0,0 +1,25 @@
+package info.rexs.model.jaxb;
+
+import info.rexs.db.constants.RexsValueType;
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
+
+/**
+ * Adapter class to convert between `Integer` and `RexsValueType` for XML binding.
+ * <p>
+ * This adapter is used to marshal and unmarshal `RexsValueType` objects to and from their numeric ID representation in XML.
+ */
+public class RexsValueTypeAdapter extends XmlAdapter<Integer, RexsValueType> {
+
+    @Override
+    public RexsValueType unmarshal(Integer v) {
+        return RexsValueType.findByNumericId(v);
+    }
+
+    @Override
+    public Integer marshal(RexsValueType v) {
+        if (v == null) {
+            return null;
+        }
+        return v.getNumericId();
+    }
+}
